e7559dfba2c1a3169b7709567d24f2c95bda0aa4
mystique4u <yurii.masalitin@powercoders.org>
2024-04-19 08:08:39 +0200

    created structure

---
+++ temp.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def get_weather(city):
+    api_key = 'YOUR_API_KEY'  # Replace with your API key
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        city_name = data['name']
+        temperature = data['main']['temp']
+        weather_desc = data['weather'][0]['description']
+        return {'city_name': city_name, 'temperature': temperature, 'weather_desc': weather_desc}
+    else:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather(city)
+    return render_template('index.html', weather_data=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
