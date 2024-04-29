b32420d4a20f8b7d3390f75ca1a9a62400c093b1
mystique4u <128178339+mystique4u@users.noreply.github.com>
2024-04-29 20:20:53 +0200

    little code optimization

---
+++ html/globe.js
@@ -0,0 +1,52 @@
+import * as THREE from '//unpkg.com/three/build/three.module.js';
+//Dark-mode checker 
+const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Define variables based on mode
+let modeVariable;
+
+if (prefersDarkMode) {
+// Assign value for dark mode
+modeVariable = '#151515';
+} else {
+// Assign value for light mode
+modeVariable = '#FFFFFF';
+}
+
+const world = Globe({ animateIn: false })
+  (document.getElementById('globe'))
+  .width(360)
+  .height(360)
+  .backgroundColor(modeVariable)
+  .pointOfView({ lat: 0, lng: 0, altitude: 1.7 })
+  .globeImageUrl('./earth-blue-marble.jpg')
+  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png');
+
+// Auto-rotate
+world.controls().autoRotate = true;
+world.controls().autoRotateSpeed = 0.35;
+
+//Responsive
+
+// window.addEventListener('resize', (event) => {
+// world.width([event.target.innerWidth])
+// world.height([event.target.innerHeight])
+// });
+
+// Add clouds sphere
+const CLOUDS_IMG_URL = './clouds.png'; // from https://github.com/turban/webgl-earth
+const CLOUDS_ALT = 0.002;
+const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
+
+new THREE.TextureLoader().load(CLOUDS_IMG_URL, cloudsTexture => {
+  const clouds = new THREE.Mesh(
+    new THREE.SphereGeometry(world.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+    new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
+  );
+  world.scene().add(clouds);
+
+  (function rotateClouds() {
+    clouds.rotation.y += CLOUDS_ROTATION_SPEED * Math.PI / 180;
+    requestAnimationFrame(rotateClouds);
+  })();
+});
