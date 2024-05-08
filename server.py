32308cea9fffd1c6cb0e308c7b6aca9ecf87c3da
mystique4u <128178339+mystique4u@users.noreply.github.com>
2024-05-08 21:08:02 +0200

    some fixes and icon fix

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 api_key = os.getenv("WAPI_KEY") # Replace with your API key
 
 def get_weather(city):
-
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
     data = response.json()
@@ -20,25 +19,30 @@ def get_weather(city):
         country_name = data['sys']['country']
         temperature = round(data['main']['temp'])
         weather_desc = data['weather'][0]['description']
-        lon = data['coord']['lon']
         lat = data['coord']['lat']
+        lon = data['coord']['lon']
         weather_icon = data['weather'][0]['icon']
 
         return {'city_name': city_name, 'country_name': country_name, 'temperature': temperature, 'weather_desc': weather_desc, 'lon': lon, 'lat': lat, 'weather_icon': weather_icon}
     else:
         return None
 
-# def get_forecast(lat, lon):
-#     url = f'http://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5&appid={api_key}'
-#     response = requests.get(url)
-#     data = response.json()
-#     if response.status_code == 200:
-#         city_name = data['name']
+def get_forecast(lat, lon):
+    url = f'http://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5&appid={api_key}'
+    lat = get_weather(lat)
+    lon = get_weather(lon)
+    response = requests.get(url)
+    data = response.json()
 
+    if response.status_code == 200:
+        # extract the data
+        day_of_week = data['list'][0]['dt']
+        icon = data['list'][1]['weather'][1]['icon']
+        description = data['list'][1]['weather'][1]['description']
 
-#         return {'city_name': city_name}
-#     else:
-#         return None
+        return {'day_of_week': day_of_week, 'icon': icon, 'description': description}
+    else:
+        return None
 
 
 @app.route('/', methods=['GET', 'POST'])
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    document.getElementById("demo").innerHTML = "Latitude: " + lat + "<br>Longitude: " + lon;
+}
     
     
