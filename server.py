7d1fa8ac7057e3d0cde9145f478f2a68ece67c24
mystique4u <128178339+mystique4u@users.noreply.github.com>
2024-05-09 17:36:39 +0200

    5 day forecast done

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import datetime
+
 
 # Other modules
 from dotenv import load_dotenv
@@ -10,11 +12,12 @@ load_dotenv()
 app = Flask(__name__)
 api_key = os.getenv("WAPI_KEY") # Replace with your API key
 
+
 def get_weather(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     response = requests.get(url)
-    data = response.json()
     if response.status_code == 200:
+        data = response.json()
         city_name = data['name']
         country_name = data['sys']['country']
         temperature = round(data['main']['temp'])
@@ -23,24 +26,37 @@ def get_weather(city):
         lon = data['coord']['lon']
         weather_icon = data['weather'][0]['icon']
 
-        return {'city_name': city_name, 'country_name': country_name, 'temperature': temperature, 'weather_desc': weather_desc, 'lon': lon, 'lat': lat, 'weather_icon': weather_icon}
+        return {'city_name': city_name, 
+                'country_name': country_name, 
+                'temperature': temperature, 
+                'weather_desc': weather_desc, 
+                'lon': lon, 
+                'lat': lat, 
+                'weather_icon': weather_icon
+                }
     else:
         return None
 
 def get_forecast(lat, lon):
-    url = f'http://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5&appid={api_key}'
-    lat = get_weather(lat)
-    lon = get_weather(lon)
+    url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric'
     response = requests.get(url)
-    data = response.json()
-
+    print(response.status_code)
     if response.status_code == 200:
+        data = response.json()
         # extract the data
-        day_of_week = data['list'][0]['dt']
-        icon = data['list'][1]['weather'][1]['icon']
-        description = data['list'][1]['weather'][1]['description']
-
-        return {'day_of_week': day_of_week, 'icon': icon, 'description': description}
+        forecast_array = []
+        for obj in data['list']:
+            dt_txt_arr = obj['dt_txt'].split(' ')
+            if dt_txt_arr[1] == '12:00:00': #data filter
+                dt_object = datetime.datetime.fromtimestamp(obj['dt'])
+                item = {
+                    'day': dt_object.strftime("%A")[: 3],
+                    'icon': obj['weather'][0]['icon'],
+                    'description': obj['weather'][0]['description'],
+                    'temperature': round(obj['main']['temp'])
+                }
+                forecast_array.append(item)
+        return forecast_array
     else:
         return None
 
@@ -52,10 +68,15 @@ def index():
 @app.route('/result', methods=['GET', 'POST'])
 def result():
     weather_data = None
+    forecast_data = None
     if request.method == 'POST':
         city = request.form['city']
         weather_data = get_weather(city)
-    return render_template('result.html', weather_data=weather_data)
+        if weather_data: 
+            lat = weather_data['lat']
+            lon = weather_data['lon']
+            to_html_forecast_data = get_forecast(lat, lon)
+    return render_template('result.html', weather_data=weather_data, forecast_data = to_html_forecast_data )
 
 if __name__ == '__main__':
     app.run(debug=True)
